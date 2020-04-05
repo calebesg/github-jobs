cdad7920264a4a32ee645c2ddb2b211a11e758db
Calebe SG <calebeguimaraes10@gmail.com>
2020-04-05 20:40:55 -0300

    altered loading screen

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,64 +1,74 @@
 import React from 'react';
-import ShimmerPlaceHolder from 'react-native-shimmer-placeholder'
-
-// import { Container } from './styles';
+import { StyleSheet, View } from 'react-native';
+import ShimmerPlaceHolder from 'react-native-shimmer-placeholder';
 
 export default function Loading({ loading }) {
   return (
-    <>
-      <ShimmerPlaceHolder visible={true} autoRun={true}>
-        <ShimmerPlaceHolder visible={loading} style={{
-          height: 20,
-          width: '100%',
-          borderRadius: 4,
-          marginVertical: 10
-        }} autoRun={true} /> 
-        <ShimmerPlaceHolder visible={loading} style={{
-          height: 60,
-          width: '80%',
-          borderRadius: 4,
-          marginBottom: 10
-        }} autoRun={true} />
-        <ShimmerPlaceHolder visible={loading} style={{
-          height: 60,
-          width: 100,
-          borderRadius: 4,
-          marginBottom: 10
-        }} autoRun={true} />
-        <ShimmerPlaceHolder visible={loading} style={{
-          height: 20,
-          width: '100%',
-          borderRadius: 4,
-          marginBottom: 30
-        }} autoRun={true} />
-      </ShimmerPlaceHolder>
-
-      <ShimmerPlaceHolder visible={true} autoRun={true}>
-        <ShimmerPlaceHolder visible={loading} style={{
-          height: 20,
-          width: '100%',
-          borderRadius: 4,
-          marginBottom: 10
-        }} autoRun={true} />
-       <ShimmerPlaceHolder visible={loading} style={{
-          height: 60,
-          width: '80%',
-          borderRadius: 4,
-          marginBottom: 10
-        }} autoRun={true} />
-        <ShimmerPlaceHolder visible={loading} style={{
-          height: 60,
-          width: 100,
-          borderRadius: 4,
-          marginBottom: 10
-        }} autoRun={true} />
-        <ShimmerPlaceHolder visible={loading} style={{
-          height: 20,
-          width: '100%',
-          borderRadius: 4,
-          marginBottom: 10
-        }} autoRun={true} />
-      </ShimmerPlaceHolder>
-    </>
+    <ShimmerPlaceHolder style={{height: 0}} visible={!loading} autoRun={true}>
+      <View style={styles.container}>
+        <ShimmerPlaceHolder 
+          visible={loading} 
+          style={styles.title} 
+          autoRun={true}
+          colorShimmer={['#ebebef88', '#c5c5cf88', '#ebebef88']}
+        /> 
+        <ShimmerPlaceHolder 
+          visible={loading} 
+          style={styles.longInfo} 
+          autoRun={true}
+          colorShimmer={['#ebebef88', '#c5c5cf88', '#ebebef88']}
+        />
+        <ShimmerPlaceHolder 
+          visible={loading}
+          style={styles.shortInfo} 
+          autoRun={true}
+          colorShimmer={['#ebebef88', '#c5c5cf88', '#ebebef88']}
+        />
+        <ShimmerPlaceHolder 
+          visible={loading} 
+          style={styles.buttom} 
+          autoRun={true}
+          colorShimmer={['#ebebef88', '#c5c5cf88', '#ebebef88']}
+        />
+      </View>
+    </ShimmerPlaceHolder>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#fff',
+    width: '100%',
+    height: 'auto',
+    borderRadius: 4,
+    padding: 20,
+    marginBottom: 16
+  },
+
+  title: {
+    height: 15,
+    width: '100%',
+    borderRadius: 10,
+    marginBottom: 20
+  },
+
+  longInfo: {
+    height: 10,
+    width: '80%',
+    borderRadius: 10,
+    marginBottom: 16
+  },
+
+  shortInfo: {
+    height: 10,
+    width: 100,
+    borderRadius: 10,
+    marginBottom: 20
+  },
+
+  buttom: {
+    height: 15,
+    width: '100%',
+    borderRadius: 10
+  }
+});
--- src/screens/Job/index.js
+++ src/screens/Job/index.js
@@ -73,7 +73,10 @@ export default function Job() {
       </View>
 
       <Loading loading={loading} />
-
+      <Loading loading={loading} />
+      <Loading loading={loading} />
+      <Loading loading={loading} />
+     
       <ShimmerPlaceHolder
         style={{height:0}}
         autoRun={true}
@@ -85,38 +88,37 @@ export default function Job() {
           keyExtractor={job => String(job.id)}
           showsVerticalScrollIndicator={false}
           renderItem={({ item: job }) => (
-          
 
-              <View style={styles.job}>
-                <Text style={styles.jobTitle}>{job.title}</Text>
-                
-                <View style={styles.jobBody}>
-                  <View style={styles.jobBodyLeft}>
-                    <Text style={styles.jobBodyTitle}>MODALIDADE:</Text>
-                    <Text style={styles.jobBodyValue}>{job.type}</Text>
+            <View style={styles.job}>
+              <Text style={styles.jobTitle}>{job.title}</Text>
+              
+              <View style={styles.jobBody}>
+                <View style={styles.jobBodyLeft}>
+                  <Text style={styles.jobBodyTitle}>MODALIDADE:</Text>
+                  <Text style={styles.jobBodyValue}>{job.type}</Text>
 
-                    <Text style={styles.jobBodyTitle}>LOCAL:</Text>
-                    <Text style={styles.jobBodyValue}>{job.location}</Text>
-                  </View>
-                  <View style={styles.jobBodyRight}>
-                    <Text style={styles.jobBodyTitle}>EMPRESA:</Text>
-                    <Image source={{ uri: job.company_logo }} style={{ 
-                      height: 50, 
-                      width: 60, 
-                      resizeMode: 'contain' ,
-                      marginTop: 5
-                    }} />
-                  </View>
+                  <Text style={styles.jobBodyTitle}>LOCAL:</Text>
+                  <Text style={styles.jobBodyValue}>{job.location}</Text>
                 </View>
+                <View style={styles.jobBodyRight}>
+                  <Text style={styles.jobBodyTitle}>EMPRESA:</Text>
+                  <Image source={{ uri: job.company_logo }} style={{ 
+                    height: 50, 
+                    width: 60, 
+                    resizeMode: 'contain' ,
+                    marginTop: 5
+                  }} />
+                </View>
+              </View>
 
-                <TouchableOpacity 
-                  onPress={() => navigateToDetail(job.url)} 
-                  style={styles.jobButton}
-                >
-                  <Text style={styles.jobButtonText}>Ver Mais</Text>
-                  <MaterialIcons name="arrow-forward" size={20} color="#1976d2" />
-                </TouchableOpacity>
-            
+              <TouchableOpacity 
+                onPress={() => navigateToDetail(job.url)} 
+                style={styles.jobButton}
+              >
+                <Text style={styles.jobButtonText}>Ver Mais</Text>
+                <MaterialIcons name="arrow-forward" size={20} color="#1976d2" />
+              </TouchableOpacity>
+          
             </View>
           )}
         />
